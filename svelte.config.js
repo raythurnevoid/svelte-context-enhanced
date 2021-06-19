ea540ece65f877089779dacef2dd532afc9a0cd5
Ray Thurne Void <raythurnevoid@gmail.com>
2021-06-19 03:54:20 +0200

    0.0.8: package update

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import preprocess from "svelte-preprocess";
 import { resolve } from "path";
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
